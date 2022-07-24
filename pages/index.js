5e4aee947b8d9fd60f8b855eab64bc71f3bf31a9
raihanmuhammad <michael.raihan@stockbit.com>
2022-07-24 20:31:26 +0700

    change to javascript and add the x table

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,44 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import { useState, useEffect } from "react";
+import parse from "html-react-parser";
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [result, setResult] = useState();
+  const [input, setInput] = useState(0);
+
+  const makeTable = (data) => {
+    let result = ``;
+    let longLength = data.length;
+    for (let i = 0; i < longLength; i++) {
+      result += `<tr>`;
+      for (let j = 0; j <= longLength - 1; j++) {
+        if (i === j) {
+          result += `<td>${data[i]}</td>`;
+        } else if (i + j === longLength - 1) {
+          result += `<td>${data[longLength - 1 - i]}</td>`;
+        } else {
+          result += `<td></td>`;
+        }
+      }
+    }
+    result += `</tr>`;
+    return result;
+  };
+
+  const makeInput = () => {
+    console.log(input);
+    for (let i = 0; i < input; i++) {
+      setData((prev) => [...prev, i + 1]);
+    }
+  };
+
+  useEffect(() => {
+    setResult(makeTable(data));
+    console.log(data);
+    // eslint-disable-next-line
+  }, [data]);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +48,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <input
+          type="number"
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="masukan angka"
+        />
+        <button onClick={makeInput}>Set</button>
+        <table border="1">{parse(`${result}`)}</table>
       </main>
 
       <footer className={styles.footer}>
@@ -58,12 +63,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
